1cc12d12ddeb06962ff35c6ed90cab201f424f5e
Unknown <vinet.jerome@gmail.com>
2019-06-21 16:52:14 +0200

    Modification du POM

--- src/main/java/com/airsante/airmes/AirmesApplication.java
+++ src/main/java/com/airsante/airmes/AirmesApplication.java
@@ -2,11 +2,14 @@ package com.airsante.airmes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy(proxyTargetClass = true)
-public class AirmesApplication {
+public class AirmesApplication
+		extends SpringBootServletInitializer
+		{
 
 	public static void main(String[] args) {
 		SpringApplication.run(AirmesApplication.class, args);
